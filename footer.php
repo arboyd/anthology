bdfec0e4b094afdb67e90ff92f193a0025ba3517
Amy R. Boyd <arboyd173@gmail.com>
2015-06-30 14:31:59 -0400

    Basic SASSy things.

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <div id="footer">
 <h1>FOOTER</h1>
 </div>
-</div>
 </body>
 </html>
--- header.php
+++ header.php
@@ -1,10 +1,10 @@
 <html>
 <head>
 	<title>Anthologthingy</title>
-	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<!--	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"> -->
+	<link href="/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
 </head>
 <body>
-	<div id="wrapper">
 	<div id="header">
 		<h1>HEADER</h1>
 	</div>
