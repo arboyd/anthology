acb20b45ddfe8bad963dee90325e80c18c636ba1
Amy R. Boyd <arboyd173@gmail.com>
2015-08-18 12:21:06 -0400

    Fixes various minor styles.

--- header.php
+++ header.php
@@ -2,10 +2,11 @@
 <head>
 	<title>Anthologthingy</title>
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	<link href = "wp-content/themes/anthology/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
-	<script src="wp-content/themes/anthology/js/jquery-1.11.3.js"></script>
-	<script src="wp-content/themes/anthology/js/open-menu.js"></script>
-	<link rel="stylesheet" href="wp-content/themes/anthology/font-awesome/css/font-awesome.min.css">
+	
+	<link href = "<?php echo get_template_directory_uri(); ?>/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<script src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.11.3.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/open-menu.js"></script>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/font-awesome/css/font-awesome.min.css">
 	
 
 </head>
